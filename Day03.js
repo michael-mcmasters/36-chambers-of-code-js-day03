d6570b0a8c42e90580cdfe243a6c834492fc423b
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2020-12-30 08:27:44 -0500

    first

---
+++ Day03.js
@@ -0,0 +1,55 @@
+class Problem {
+
+    /**
+     * Have the function SimpleEvens(num) check whether every single number in the passed in parameter is even. 
+     * If so, return the string true, otherwise return the string false. 
+     * 
+     * For example: if num is 4602225 your program should return the string false because 5 is not an even number.
+     */
+    simpleEvens(num) {
+        // code goes here  
+        return num;
+    }
+
+    /**
+     * Have the function QuestionsMarks(str) take the str string parameter, which will contain single digit numbers, letters, and question marks, 
+     * and check if there are exactly 3 question marks between every pair of two numbers that add up to 10. If so, then your program should return 
+     * the string true, otherwise it should return the string false. If there aren't any two numbers that add up to 10 in the string, then your program 
+     * should return false as well.
+     * 
+     * For example: if str is "arrb6???4xxbl5???eee5" then your program should return true because there are exactly 3 question marks between 6 and 4, 
+     * and 3 question marks between 5 and 5 at the end of the string.
+     */
+    questionsMarks(str) {
+        // code goes here  
+        return null;
+    }
+
+    /**
+     * Have the function SnakeCase(str) take the str parameter being passed and return it in proper snake case format where each 
+     * word is lowercased and separated from adjacent words via an underscore. The string will only contain letters and some 
+     * combination of delimiter punctuation characters separating each word.
+     * 
+     * For example: if str is "BOB loves-coding" then your program should return the string bob_loves_coding.
+     */
+    snakeCase(str) {
+        // code goes here  
+        return null;
+    }
+
+    /**
+     * Have the function GCF(arr) take the array of numbers stored in arr which will always contain only 
+     * two positive integers, and return the greatest common factor of them. 
+     * 
+     * For example: if arr is [45, 12] then your program should return 3. 
+     * There will always be two elements in the array and they will be positive integers.
+     */
+    gcf(arr) {
+        // code goes here  
+        return null;
+      }
+
+
+}
+
+module.exports = Problem;
---
+++ Day03.test.js
@@ -0,0 +1,73 @@
+const { TestScheduler } = require('jest');
+const Problem = require('./Day03');
+
+test("SimpleEvens test01", () => {
+
+    let problem = new Problem();
+    let expected = true;
+
+    let actual = problem.simpleEvens(2222220222);
+
+    expect(actual).toEqual(expected);
+});
+
+test("SimpleEvens test02", () => {
+
+    let problem = new Problem();
+    let expected = false;
+
+    let actual = problem.simpleEvens(20864646452);
+
+    expect(actual).toEqual(expected);
+});
+
+
+test("QuestionsMarks test01", () => {
+
+    let problem = new Problem();
+    let expected = false;
+
+    let actual = problem.questionsMarks("aa6?9");
+
+    expect(actual).toEqual(expected);
+});
+
+test("QuestionsMarks test01", () => {
+
+    let problem = new Problem();
+    let expected = "cats_and_dogs_are_awesome";
+
+    let actual = problem.questionsMarks("cats AND*Dogs-are Awesome");
+
+    expect(actual).toEqual(expected);
+});
+
+test("snakeCase test02", () => {
+
+    let problem = new Problem();
+    let expected = "a_b_c_d_e_f_g";
+
+    let actual = problem.snakeCase("a b c d-e-f%g");
+
+    expect(actual).toEqual(expected);
+});
+
+test("gcf test01", () => {
+
+    let problem = new Problem();
+    let expected = 1;
+
+    let actual = problem.gcf([1, 6]);
+
+    expect(actual).toEqual(expected);
+});
+
+test("gcf test02", () => {
+
+    let problem = new Problem();
+    let expected = 4;
+
+    let actual = problem.gcf([12, 28]);
+
+    expect(actual).toEqual(expected);
+});
